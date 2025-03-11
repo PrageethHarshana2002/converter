5eb439c312940b791f62fa3a2b7fbbbf325aced2
prageeth <prageeth.har2002@gmail.com>
2025-03-11 14:21:44 +0530

    i modified the temperature_operations.py file creating functions to operate convertion under the TemperatureConverter class

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     print("1. Celsius to Fahrenheit")
     print("2. Fahrenheit to Celsius")
     print("3. Celsius to Kelvin")
-    print("4. Kelvin to Celsius")
 
-    choice = input("Enter choice (1/2/3/4): ")
+
+    choice = input("Enter choice (1/2/3): ")
---
+++ temperature_operations.py
@@ -0,0 +1,10 @@
+class TemperatureConverter:
+    def celsius_to_fahrenheit(self, celsius):
+        return (celsius * 9/5) + 32
+
+    def fahrenheit_to_celsius(self, fahrenheit):
+        return (fahrenheit - 32) * 5/9
+
+    def celsius_to_kelvin(self, celsius):
+        return celsius + 273.15
+
